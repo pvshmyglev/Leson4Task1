1a813a68b0af5d7bf79a1bc9dd0057faa5969c33
pvshmyglev <pshm2@mail.ru>
2022-02-20 20:22:50 +0300

    All Right test

--- src/test/kotlin/ru/netology/MainKtTest.kt
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -233,7 +233,7 @@ class MainKtTest {
     @Test
     fun calculateCommissionDefaultOldAmount() {
 
-        val expectedValue = 1
+        val expectedValue = 0
 
         val sumAmount : Int = 550
         val sumOldAmount : Int = 7490000
